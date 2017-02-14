718683cfc7f23946c7ef16d8388631eee7c9eaa9
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-02-14 15:35:38 -0600

    Added Background Image,Ball, and Two Colliders

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class MusicPlayer : MonoBehaviour {
 
 	 static MusicPlayer instance = null;
-
-	// Use this for initialization
-	void Start () {
+	 
+	 
+	 void Awake(){
+	 	//Debug.Log ("Music plyer Awaked: " + GetInstanceID());
 		if(instance !=null){
 			Destroy (gameObject);
 			print ("Duplicate music player:self-destructing");
@@ -14,6 +15,11 @@ public class MusicPlayer : MonoBehaviour {
 			instance=this;
 			GameObject.DontDestroyOnLoad(gameObject);//This Command is used to keep music playing throughout differnt scenes
 		}
+	 }
+
+	// Use this for initialization
+	void Start () {
+		//Debug.Log ("Music plyer Start: " + GetInstanceID());
 	}
 	
 	// Update is called once per frame
