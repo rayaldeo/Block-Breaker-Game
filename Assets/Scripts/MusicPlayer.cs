4f77dada28838d337bfaacdce196a37f8c80bec6
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-02-13 21:47:14 -0600

    Added Music to all scenes

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	 static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void Start () {
+		if(instance !=null){
+			Destroy (gameObject);
+			print ("Duplicate music player:self-destructing");
+		}else{
+			instance=this;
+			GameObject.DontDestroyOnLoad(gameObject);//This Command is used to keep music playing throughout differnt scenes
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
