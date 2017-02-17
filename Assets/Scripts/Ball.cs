7d03fee09de8ff7b5ba44305326207b079d86536
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-02-16 19:45:48 -0600

    The ball stays on paddle until mouse click. AND...ball stays within the play space

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Paddle paddle;
+	private bool hasStarted=false;
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector=this.transform.position - paddle.transform.position;
+		print(paddleToBallVector);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!hasStarted){
+			//Lock the ball relative to the paddle
+			this.transform.position = paddle.transform.position+paddleToBallVector;
+		//Waiting for a Mouse Press for a Launch
+		if(Input.GetMouseButtonDown(0)){
+			print ("Mouse Clicked");
+			hasStarted=true;
+			this.rigidbody2D.velocity = new Vector2(2f,10f);
+		}
+		}
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public float mousePosInBlocks;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 paddlePos = new Vector3(this.transform.position.x,this.transform.position.y,0f);
+		//this.transform.position.y->This function will keep the y variable set in the
+		//inspecter for the object
+		mousePosInBlocks=Mathf.Clamp(Input.mousePosition.x/Screen.width*16,1f,15f);
+		print (mousePosInBlocks);
+		paddlePos.x =mousePosInBlocks;
+		this.transform.position=paddlePos;
+	
+	}
+}
