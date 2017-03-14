e3ae9a89fcc4211b2f4c7182d40e8638ab7d456c
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-03-13 23:24:10 -0500

    Added a New Paddle,try to resolve boring vertical/horizontal bug, Rename Levels,Tweaked Scripts

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -12,7 +12,7 @@ public class Ball : MonoBehaviour {
 	void Start () {
 		//This is how we programatically find an object when creating a new leve
 		paddle = GameObject.FindObjectOfType<Paddle>();
-		paddleToBallVector=this.transform.position - paddle.transform.position;
+		paddleToBallVector=this.transform.position - paddle.transform.position;//This is needed to calculate the proper space between ball and paddle
 		//print(paddleToBallVector);
 	}
 	
@@ -20,7 +20,8 @@ public class Ball : MonoBehaviour {
 	void Update () {
 		if(!hasStarted){
 			//Lock the ball relative to the paddle
-			this.transform.position = paddle.transform.position+paddleToBallVector;
+			this.transform.position = paddle.transform.position+paddleToBallVector;//The variable paddleToBallVector is added into the paddle position to act as
+																					//ball is resting on the paddle
 		//Waiting for a Mouse Press for a Launch
 		if(Input.GetMouseButtonDown(0)){
 			//print ("Mouse Clicked");
@@ -32,10 +33,11 @@ public class Ball : MonoBehaviour {
 	
 	void OnCollisionEnter2D(Collision2D col){
 		//This line of code is used to stop the boring vertical/horizontal loop from happening
-		Vector2 tweak = new Vector2(Random.Range(0f,0.2f),Random.Range (0f,0.2f));
+		Vector2 tweak = new Vector2(Random.Range(0.1f,0.2f),Random.Range (0f,0.2f));
+		//Vector3 tweakDirection = new Vector3(Random.Range(0f,0.2f),0f,0f);
 		if(hasStarted){
 			audio.Play ();
 			rigidbody2D.velocity+=tweak;
-			}
+		}
 	}
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,6 +5,8 @@ public class Paddle : MonoBehaviour {
 	private Ball ball;
 	public bool autoPlayCheck=false;
 	public float mousePosInBlocks;
+	private float minX=1.75f;
+	private float maxX=14.4f;
 
 	// Use this for initialization
 	void Start () {
@@ -26,7 +28,7 @@ public class Paddle : MonoBehaviour {
 		Vector3 paddlePos = new Vector3(this.transform.position.x,this.transform.position.y,0f);
 		//this.transform.position.y->This function will keep the y variable set in the
 		//inspecter for the object
-		mousePosInBlocks=Mathf.Clamp(Input.mousePosition.x/Screen.width*16,1f,15f);
+		mousePosInBlocks=Mathf.Clamp(Input.mousePosition.x/Screen.width*16,minX,maxX);
 		//print (mousePosInBlocks);
 		paddlePos.x =mousePosInBlocks;
 		this.transform.position=paddlePos;
@@ -37,7 +39,7 @@ public class Paddle : MonoBehaviour {
 		Vector3 paddlePos = new Vector3(this.transform.position.x,this.transform.position.y,0f);
 		//this.transform.position.y->This function will keep the y variable set in the
 		Vector3 ballPos = ball.transform.position;
-		paddlePos.x =Mathf.Clamp(ballPos.x,0.5f,15.5f);
+		paddlePos.x =Mathf.Clamp(ballPos.x,minX,maxX);
 		this.transform.position=paddlePos;
 		
 	}
